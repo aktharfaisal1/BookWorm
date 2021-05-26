44ac178827fe467d6c6ab3ed0c195275dbd78ee6
aktharfaisal1 <aktharfaisal1@gmail.com>
2021-05-26 20:20:20 +0300

    Add all my files

---
+++ wwwroot/js/siteee.js
@@ -0,0 +1,33 @@
+﻿/* MAIN PAGE */
+$(function () {
+    $(".card-image-js").animate({ opacity: 0.8 });
+    $(".btn-style").animate({ opacity: 0.8 });
+});
+$(".pic1").on("mouseover", function () {
+    $(this).animate({ opacity: 1 });
+});
+
+$(".pic1").mouseleave(function () {
+    $(".pic1").animate({ opacity: 0.7 });
+
+});
+$(".signin,.add-new").click(function () {
+
+    $("#modelId").modal('show');
+});
+$(".card-image-js,.btn-style,.userPic").on("mouseover", function () {
+    $(this).animate({ opacity: 1 });
+});
+
+$(".card-image-js,.btn-style,.userPic").mouseleave(function () {
+    $(this).animate({ opacity: 0.8 });
+});
+
+$(".top-items").mouseover(function () {
+    $(this).css("background-color", "#dba84f;");
+});
+
+$(".top-items").mouseleave(function () {
+    $(this).css("background-color", "white");
+});
+
